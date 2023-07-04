884f6e7b28637f37493cbfe2b6c5801bcc475dd6
Leonardo Luize <leonardo.luize2@gmail.com>
2023-07-03 22:20:28 -0300

    feat: add authentication check component

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { user } from "$lib/firebase";
+</script>
+
+{#if $user}
+  <slot />
+{:else}
+  <div class="flex flex-col items-center justify-center gap-3">
+    <p class="text-error">You must be signed in to view this page.</p>
+    <a class="btn btn-neutral btn-outline" href="/login">Sign In</a>
+  </div>
+{/if}
